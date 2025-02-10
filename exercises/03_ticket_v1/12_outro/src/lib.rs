45476cb308f220973bf979870aec1dada9e35134
naoki.kiriya@unitec-ccs.co.jp <naoki.kiriya>
2025-02-10 16:46:01 +0900

    3-12

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -1,3 +1,5 @@
+use core::panic;
+
 // TODO: Define a new `Order` type.
 //   It should keep track of three pieces of information: `product_name`, `quantity`, and `unit_price`.
 //   The product name can't be empty and it can't be longer than 300 bytes.
@@ -11,3 +13,68 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32
+}
+
+impl Order {
+    pub fn total(&self) -> u32 {
+        &self.quantity * &self.unit_price
+    }
+    
+    fn check_product_name(name: &String) {
+        if name.is_empty() {
+            panic!()
+        }
+        if name.len() > 300 {
+            panic!()
+        }
+    }
+    fn check_quantity(quantity: u32) {
+        if quantity == 0 {
+            panic!()
+        }
+    }
+
+    fn check_unit_price(unit_price: u32) {
+        if unit_price == 0 {
+            panic!()
+        }
+    }
+    
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order {
+        Self::check_unit_price(unit_price);
+        Self::check_quantity(quantity);
+        Self::check_product_name(&product_name);
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn set_unit_price(&mut self, price: u32) {
+        self.unit_price = price;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        self.quantity = quantity;
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        self.product_name = product_name;
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+    
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+}
